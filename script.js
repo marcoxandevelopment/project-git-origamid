95d10a363f86747db434a60ae15c71fb8d52fa4f
Marco Antônio <Marcoaz651@gmail.com>
2023-06-30 09:55:47 -0300

    Primeiro commit

---
+++ script.js
@@ -0,0 +1,90 @@
+ScrollReveal({
+  reset: true,
+  distance: "200px",
+  duration: 1000,
+  delay: 0,
+});
+
+ScrollReveal().reveal(".container-main-content", {
+  delay: 800,
+});
+
+ScrollReveal().reveal(".container-work-text", {
+  delay: 300,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".img1", {
+  delay: 100,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".img2", {
+  delay: 200,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".img3", {
+  delay: 300,
+  origin: "left",
+});
+ScrollReveal().reveal(".img4", {
+  delay: 400,
+  origin: "left",
+});
+ScrollReveal().reveal(".img5", {
+  delay: 500,
+  origin: "left",
+});
+ScrollReveal().reveal(".img6", {
+  delay: 600,
+  origin: "left",
+});
+ScrollReveal().reveal(".img7", {
+  delay: 700,
+  origin: "left",
+});
+ScrollReveal().reveal(".img8", {
+  delay: 800,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".how-orks-img", {
+  delay: 400,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".how-works-text", {
+  delay: 700,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".how-works-text-p", {
+  delay: 1000,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".container-text-grid", {
+  delay: 800,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".container-grid-img", {
+  delay: 400,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".talk-with-us-h1", {
+  delay: 600,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".talk-width-us-p", {
+  delay: 1000,
+  origin: "bottom",
+});
+
+ScrollReveal().reveal(".ctn-act", {
+  delay: 800,
+  origin: "rigth",
+});
